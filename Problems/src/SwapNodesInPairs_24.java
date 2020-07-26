bf41f19420e0055df4ae562c11375f8fd8aef832
wang-zhenqi <dreamingwzq@gmail.com>
2020-07-26 15:56:35 +0800

    [add] source code of problem 24 - swap nodes in pairs. version 1, scanning the linked list twice.

---
+++ Problems/src/SwapNodesInPairs_24.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes,
+ * only nodes itself may be changed.
+ *
+ * Example:
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class SwapNodesInPairs_24 {
+    private ListNode result;
+
+    /**
+     * Version 1. Break the linked list into two, and than merge them together.
+     * This method has the time complexity of O(2n). It could be better.
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        if(head.next == null) {
+            return head;
+        }
+
+        ListNode first = head, second = head.next;
+        ListNode cur1 = first, cur2 = second;
+        // separate
+        while((cur1 != null && cur1.next != null) ||
+                (cur2 != null && cur2.next != null)) {
+            if(cur1 != null && cur1.next != null) {
+                cur1.next = cur1.next.next;
+                cur1 = cur1.next;
+            }
+            if(cur2 != null && cur2.next != null) {
+                cur2.next = cur2.next.next;
+                cur2 = cur2.next;
+            }
+        }
+
+        // merge
+        ListNode tail = second;
+        result = second;
+        while(second != null && first != null) {
+            ListNode n2 = second.next;
+            second = second.next;
+
+            tail.next = first;
+            first = first.next;
+            tail = tail.next;
+
+            if(n2 != null) {
+                tail.next = n2;
+                tail = tail.next;
+            }
+        }
+
+        return result;
+    }
+}
+
+class MainClass_24 {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+
+            ListNode ret = new SwapNodesInPairs_24().swapPairs(head);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
