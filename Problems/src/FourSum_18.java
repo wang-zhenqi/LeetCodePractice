c04fd78f546fafad3ad41445a755f8e0c6053184
wang-zhenqi <dreamingwzq@gmail.com>
2020-07-30 00:12:05 +0800

    [add] source code of problem 18 - four sum. Similar solution to ThreeSum_15.

---
+++ Problems/src/FourSum_18.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Given an array nums of n integers and an integer target, are there elements
+ * a, b, c, and d in nums such that a + b + c + d = target?
+ * Find all unique quadruplets in the array which gives the sum of target.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/4sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FourSum_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(nums.length <= 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 3; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for(int j = i + 1; j < nums.length - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1, l = nums.length - 1;
+                while(k < l){
+                    if(nums[i] + nums[j] + nums[k] + nums[l] > target) {
+                        l--;
+                    } else if(nums[i] + nums[j] + nums[k] + nums[l] < target) {
+                        k++;
+                    } else {
+                        List<Integer> quadruplet = new ArrayList<>();
+                        quadruplet.add(nums[i]);
+                        quadruplet.add(nums[j]);
+                        quadruplet.add(nums[k]);
+                        quadruplet.add(nums[l]);
+                        result.add(quadruplet);
+
+                        while(k < l && nums[k] == nums[k + 1]) {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l - 1]) {
+                            l--;
+                        }
+                        k++;
+                        l--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
+class MainClass_18 {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            line = in.readLine();
+            int target = Integer.parseInt(line);
+
+            List<List<Integer>> ret = new FourSum_18().fourSum(nums, target);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
