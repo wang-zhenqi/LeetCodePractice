7bffb8b8907eb097bccf39c721f563cb64042409
wang-zhenqi <dreamingwzq@gmail.com>
2020-07-29 18:11:58 +0800

    [add] source code of problem 15 - three sum. One loop with two ended searching.

--- Problems/src/ThreeSum_15.java
+++ Problems/src/ThreeSum_15.java
@@ -1,2 +1,129 @@
-package PACKAGE_NAME;public class ThreeSum_15 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums
+ * such that a + b + c = 0? Find all unique triplets in the array which
+ * gives the sum of zero.
+ *
+ * Note:
+ *
+ * The solution set must not contain duplicate triplets.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ThreeSum_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        /*if(nums.length <= 2) {
+            return result;
+        }*/
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1, k = nums.length - 1;
+            while(j < k) {
+                if(nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else if(nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]);
+                    result.add(triplet);
+                    while(j < k) {
+                        boolean f1 = false, f2 = false;
+                        if(nums[j] == nums[j + 1]) {
+                            j++;
+                        } else {
+                            f1 = true;
+                        }
+                        if(nums[k] == nums[k - 1]) {
+                            k--;
+                        } else {
+                            f2 = true;
+                        }
+                        if(f1 && f2) {
+                            j++;
+                            k--;
+                            break;
+                        }
+                    }
+                }
+            }
+            while(nums[i] == nums[i + 1] && i < nums.length - 2) {
+                i++;
+            }
+        }
+        return result;
+    }
+}
+
+class MainClass_15 {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            List<List<Integer>> ret = new ThreeSum_15().threeSum(nums);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
 }
