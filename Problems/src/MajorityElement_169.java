d893b7189439b421202d1768b1bc5e6f42ec3972
wang-zhenqi <dreamingwzq@gmail.com>
2020-07-31 22:13:53 +0800

    [modify] MajorityElement_169, using boyer-moore voting algorithm.

--- Problems/src/MajorityElement_169.java
+++ Problems/src/MajorityElement_169.java
@@ -16,8 +16,20 @@ import java.util.Arrays;
  */
 public class MajorityElement_169 {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int candidate = nums[0];
+        int vote = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != candidate) {
+                vote--;
+                if(vote <= 0) {
+                    candidate = nums[i];
+                    vote = 1;
+                }
+            } else {
+                vote++;
+            }
+        }
+        return candidate;
     }
 
     private static int[] stringToIntegerArray(String input) {
