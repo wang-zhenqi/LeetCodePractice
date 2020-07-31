28c9c4675d8c03412db30b4b10495317abfe9e69
wang-zhenqi <dreamingwzq@gmail.com>
2020-07-31 21:07:16 +0800

    [add] source code of problem 169 - majority element, using hash map to count for the majority.

---
+++ Problems/src/MajorityElement_169.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is
+ * the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always
+ * exist in the array.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MajorityElement_169 {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            Integer count;
+            if((count = map.get(nums[i])) != null) {
+                map.put(nums[i], count + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+            if(map.get(nums[i]) > nums.length / 2) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+
+    private static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            int ret = new MajorityElement_169().majorityElement(nums);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
