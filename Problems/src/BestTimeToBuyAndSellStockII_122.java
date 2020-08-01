05b96983981e42dcfd6618eeb8db089c5bfcecbe
wang-zhenqi <dreamingwzq@gmail.com>
2020-08-01 19:43:09 +0800

    [modify] BestTimeToBuyAndSellStockII_122, simplified DP, use constant number of status record.

--- Problems/src/BestTimeToBuyAndSellStockII_122.java
+++ Problems/src/BestTimeToBuyAndSellStockII_122.java
@@ -20,30 +20,25 @@ import java.io.InputStreamReader;
 public class BestTimeToBuyAndSellStockII_122 {
     public int maxProfit(int[] prices) {
         /*
-         * Version 3, DP
-         * dp[i][s] represents the maximum profit by day i, and the status on that day
-         * is s, where s = {0 (no stock held) | 1 (stock held)}.
-         *
-         * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-         * dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
-         *
-         * return dp[n-1][0]
+         * Version 4, simplified DP, time: O(N), space: O(1).
          */
 
         if(prices.length == 0) {
             return 0;
         }
 
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+        int notHeld = 0, held;
+        int preNotHeld = 0, preHeld = -prices[0];
 
         for(int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            notHeld = Math.max(preNotHeld, preHeld + prices[i]);
+            held = Math.max(preHeld, preNotHeld - prices[i]);
+
+            preHeld = held;
+            preNotHeld = notHeld;
         }
 
-        return dp[prices.length - 1][0];
+        return notHeld;
     }
 
     private static int[] stringToIntegerArray(String input) {
