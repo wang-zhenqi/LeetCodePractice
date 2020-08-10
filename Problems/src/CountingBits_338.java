d2bd986244f86aaafbca2b034304e44031843f26
wang-zhenqi <dreamingwzq@gmail.com>
2020-08-10 12:24:45 +0800

    [add] source code of problem 338 - counting bits. DP

---
+++ Problems/src/CountingBits_338.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Given a non negative integer number num. For every numbers i in the range
+ * 0 ≤ i ≤ num calculate the number of 1's in their binary representation and
+ * return them as an array.
+ *
+ * Follow up:
+ * It is very easy to come up with a solution with run time O(n*sizeof(integer)).
+ * But can you do it in linear time O(n) / possibly in a single pass?
+ * Space complexity should be O(n).
+ * Can you do it like a boss? Do it without using any builtin function like
+ * __builtin_popcount in c++ or in any other language.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/counting-bits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CountingBits_338 {
+    private static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    private static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int num = Integer.parseInt(line);
+
+            int[] ret = new CountingBits_338().countBits(num);
+
+            String out = integerArrayToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public int[] countBits(int num) {
+        int[] count = new int[num + 1];
+        count[0] = 0;
+        int power = -1;
+        for(int i = 1; i <= num; i++) {
+            if((i & (i - 1)) == 0) {
+                power++;
+            }
+            count[i] = count[~(1 << power) & i] + 1;
+        }
+        return count;
+    }
+}
