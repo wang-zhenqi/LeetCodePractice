65ebf4a5e456b6caba11229749be4125ff60e4f9
wang-zhenqi <dreamingwzq@gmail.com>
2020-08-13 20:40:57 +0800

    [modify] LongestIncreasingSubsequence_300, version two, a tricky method, actually practising binary search.

--- Problems/src/LongestIncreasingSubsequence_300.java
+++ Problems/src/LongestIncreasingSubsequence_300.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /**
  * Given an unsorted array of integers, find the length of longest increasing
@@ -20,6 +19,37 @@ import java.util.Arrays;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LongestIncreasingSubsequence_300 {
+    /**
+     * Insert the element ino the array lis in increasing order.
+     * @param lis the longest increasing subsequence, so far.
+     * @param length the last element that is actually in the lis.
+     * @param element the element to be inserted.
+     * @return return the actual current length of the lis.
+     */
+    private int binaryInsert(int[] lis, int length, int element) {
+        if(length == 0) {
+            lis[0] = element;
+            return 1;
+        }
+
+        int leftBound = 0, midElement, rightBound = length - 1;
+        while(leftBound < rightBound) {
+            midElement = leftBound + (rightBound - leftBound) / 2;
+            if(lis[midElement] >= element) {
+                rightBound = midElement;
+            } else {
+                leftBound = midElement + 1;
+            }
+        }
+        if(lis[rightBound] < element) {
+            lis[rightBound + 1] = element;
+            return length + 1;
+        } else {
+            lis[rightBound] = element;
+            return length;
+        }
+    }
+
     private static int[] stringToIntegerArray(String input) {
         input = input.trim();
         input = input.substring(1, input.length() - 1);
@@ -52,16 +82,28 @@ public class LongestIncreasingSubsequence_300 {
 
     public int lengthOfLIS(int[] nums) {
         /*
-         * Version 1, DP with pruning.
-         * Assume that subSeqLen[i] is the length of the longest increasing
-         * subsequence which starts from nums[i].
+         * Version 2, a tricky method.
+         * Define a array called lis(longest increasing subsequence) to simulate
+         * the process of finding the LIS.
+         * The array lis stores the possible subsequence, but not necessarily the
+         * exact LIS of the whole array.
+         * The basic idea is trying to put the smallest elements into the array in
+         * the right order.
          *
-         * subSeqLen[i] = max{subSeqLen[j]} + 1, where j > i && nums[j] > nums[i].
+         * The process is as follows:
+         * for i in 0 to nums.length - 1:
+         *     if lis is empty:
+         *         lis.add(nums[i])
+         *     else:
+         *         if nums[i] > all elements in lis:
+         *             lis.add(nums[i])
+         *         else:
+         *             // This step use binary search to reduce the time complexity
+         *             replace the smallest element in lis that is larger than nums[i]
+         * return lis.length
          *
-         * result = max{subSeqLen[i]}, where i is from 0 to nums.length - 1.
-         *
-         * initialize:
-         * subSeqLen[i] = 1
+         * The reason why this method can work is that replacing an element with a
+         * smaller one doesn't affect the length of lis.
          */
         if(nums == null) {
             return 0;
@@ -71,26 +113,12 @@ public class LongestIncreasingSubsequence_300 {
             return len;
         }
 
-        int[] subSeqLen = new int[len];
-        Arrays.fill(subSeqLen, 1);
-
-        int max = 1, nearest;
-        for(int i = len - 2; i >= 0; i--) {
-            /*
-             * nearest = min{nums[j]}, where j > i && nums[j] > nums[i].
-             * By defining this variable, the operation will be simplified, but the
-             * time complexity won't change.
-             */
-            nearest = Integer.MAX_VALUE;
-            for(int j = i + 1; j < len; j++) {
-                if(nums[j] > nums[i] && nums[j] < nearest) {
-                    subSeqLen[i] = Math.max(subSeqLen[i], 1 + subSeqLen[j]);
-                    nearest = nums[j];
-                }
-            }
-            max = Math.max(max, subSeqLen[i]);
+        int[] lis = new int[len];
+        int currentLen = 0;
+        for(int num : nums) {
+            currentLen = binaryInsert(lis, currentLen, num);
         }
 
-        return max;
+        return currentLen;
     }
 }
