c261680cafb6ee4cd676f9ac9ba6e15ae54191b1
wang-zhenqi <dreamingwzq@gmail.com>
2020-08-18 12:57:03 +0800

    [modify] CutRope_14_I, PathInAMatrix_12

--- LCOF/src/PathInAMatrix_12.java
+++ LCOF/src/PathInAMatrix_12.java
@@ -1,40 +1,38 @@
-import java.util.HashSet;
-
 /**
  * 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一格开始，每
  * 一步可以在矩阵中向左、右、上、下移动一格。如果一条路径经过了矩阵的某一格，那么该路径不能再次进入该格子。例如，
  * 在下面的3×4的矩阵中包含一条字符串“bfce”的路径（路径中的字母用加粗标出）。
- *
+ * <p>
  * [["a","b","c","e"],
  * ["s","f","c","s"],
  * ["a","d","e","e"]]
- *
+ * <p>
  * 但矩阵中不包含字符串“abfb”的路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入
  * 这个格子。
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= board.length <= 200
  * 1 <= board[i].length <= 200
  * 注意：本题与主站 79 题相同：https://leetcode-cn.com/problems/word-search/
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class PathInAMatrix_12 {
-    private int rows;
-    private int cols;
     private final int[] dr = {-1, 1, 0, 0};
     private final int[] dc = {0, 0, -1, 1};
+    private int rows;
+    private int cols;
     private char[] word;
     private char[][] board;
 
     public static void main(String[] args) {
         char[][] board = {
-                {'A','B','C','E'},
-                {'S','F','C','S'},
-                {'A','D','E','E'}
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
         };
         /*char[][] board = {
                 {'A'}
