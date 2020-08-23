a8baac7be937b4aa226f4a9076dc51377aac8e00
wang-zhenqi <dreamingwzq@gmail.com>
2020-08-23 11:50:30 +0800

    [modify] MergeTwoSortedLists_21, iteration solution.

--- Problems/src/MergeTwoSortedLists_21.java
+++ Problems/src/MergeTwoSortedLists_21.java
@@ -71,19 +71,25 @@ public class MergeTwoSortedLists_21 {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        //recursion
-        if(l1 == null) {
-            return l2;
+        //iteration
+        ListNode dummyHead = new ListNode(0);
+        ListNode curNode = dummyHead;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                curNode.next = l1;
+                l1 = l1.next;
+            } else {
+                curNode.next = l2;
+                l2 = l2.next;
+            }
+            curNode = curNode.next;
         }
-        if(l2 == null) {
-            return l1;
+        if(l1 != null) {
+            curNode.next = l1;
+        } else {
+            curNode.next = l2;
         }
-        if(l1.val <= l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        }
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
+        return dummyHead.next;
     }
 
     //Definition for singly-linked list.
