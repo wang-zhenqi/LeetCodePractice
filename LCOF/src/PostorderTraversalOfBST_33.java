85ffd4588d76ee52c23b68ce27a17743b49f7bc2
wang-zhenqi <dreamingwzq@gmail.com>
2020-09-01 11:33:58 +0800

    [modify] PostorderTraversalOfBST_33, recursion, no need to use inorder sequence, just separate the left and right child, judging them respectively.

--- LCOF/src/PostorderTraversalOfBST_33.java
+++ LCOF/src/PostorderTraversalOfBST_33.java
@@ -1,27 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /**
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回true，否则返回false。
  * 假设输入的数组的任意两个数字都互不相同。
- *
+ * <p>
  * 提示：数组长度 <= 1000
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class PostorderTraversalOfBST_33 {
-    private int[] postorder;
-    private int[] inorder;
-    private int postIdx;
-
     public static int[] stringToIntegerArray(String input) {
         input = input.trim();
         input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
+        if(input.length() == 0) {
             return new int[0];
         }
 
@@ -41,7 +36,7 @@ public class PostorderTraversalOfBST_33 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
-        while ((line = in.readLine()) != null) {
+        while((line = in.readLine()) != null) {
             int[] postorder = stringToIntegerArray(line);
 
             boolean ret = new PostorderTraversalOfBST_33().verifyPostorder(postorder);
@@ -53,40 +48,35 @@ public class PostorderTraversalOfBST_33 {
     }
 
     public boolean verifyPostorder(int[] postorder) {
-        if(postorder.length == 0) {
-            return true;
-        }
-        this.postorder = postorder;
-        inorder = Arrays.copyOf(postorder, postorder.length);
-        Arrays.sort(inorder);
-        postIdx = postorder.length - 1;
-
-        return (findRoot(0, inorder.length - 1));
+        return verifyPostorder(postorder, 0, postorder.length - 1);
     }
 
-    private boolean findRoot(int inorderBegin, int inorderEnd) {
-        int rootIdx = -1;
-        for(int i = inorderBegin; i <= inorderEnd; i++) {
-            if(inorder[i] == postorder[postIdx]) {
-                rootIdx = i;
-                break;
+    private boolean verifyPostorder(int[] postorder, int begin, int end) {
+        if(begin == end) {
+            return true;
+        }
+        int rightChildBegin = -1;
+        for(int i = begin; i < end; i++) {
+            if(rightChildBegin == -1 && postorder[i] > postorder[end]) {
+                rightChildBegin = i;
             }
-        }
-        if(rootIdx == -1) {
-            return false;
-        }
-        int rChildLeft = rootIdx + 1, rChildRight = inorderEnd;
-        if(rChildLeft <= rChildRight && postIdx - 1 >= 0){
-            postIdx--;
-            if(!findRoot(rChildLeft, rChildRight)) {
+            if(rightChildBegin != -1 && postorder[i] < postorder[end]) {
                 return false;
             }
         }
-        rChildLeft = inorderBegin;
-        rChildRight = rootIdx - 1;
-        if(rChildLeft <= rChildRight && postIdx - 1 > 0) {
-            postIdx--;
-            return findRoot(rChildLeft, rChildRight);
+        if(rightChildBegin == -1) {
+            rightChildBegin = end;
+        }
+        int childBegin = rightChildBegin, childEnd = end - 1;
+        if(childBegin <= childEnd) {
+            if(!verifyPostorder(postorder, childBegin, childEnd)) {
+                return false;
+            }
+        }
+        childBegin = begin;
+        childEnd = rightChildBegin - 1;
+        if(childBegin <= childEnd) {
+            return verifyPostorder(postorder, childBegin, childEnd);
         }
         return true;
     }
