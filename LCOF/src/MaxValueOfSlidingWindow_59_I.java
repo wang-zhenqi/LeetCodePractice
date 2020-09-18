cc58e672db29e20e7052608f0e6fdafb59ba5136
wang-zhenqi <dreamingwzq@gmail.com>
2020-09-18 13:14:30 +0800

    [add] problem 59 - I of LCOF, deque, storing the index of each element, rather than their values.

---
+++ LCOF/src/MaxValueOfSlidingWindow_59_I.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ * <p>
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * <p>
+ * 提示：
+ * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+ * <p>
+ * 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxValueOfSlidingWindow_59_I {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if(input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if(length == 0) {
+            return "[]";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result.append(number).append(", ");
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            line = in.readLine();
+            int k = Integer.parseInt(line);
+
+            int[] ret = new MaxValueOfSlidingWindow_59_I().maxSlidingWindow(nums, k);
+
+            String out = integerArrayToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> windowIndices = new LinkedList<>();
+        int i = 0;
+        for(int curIndex = 0; curIndex < nums.length; curIndex++) {
+            if(!windowIndices.isEmpty() && curIndex - windowIndices.peekFirst() == k) {
+                windowIndices.pollFirst();
+            }
+
+            while(!windowIndices.isEmpty() && nums[curIndex] > nums[windowIndices.peekLast()]) {
+                windowIndices.pollLast();
+            }
+
+            windowIndices.offerLast(curIndex);
+            if(curIndex >= k - 1) {
+                result[i++] = nums[windowIndices.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
