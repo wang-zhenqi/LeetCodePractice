4395d77e1fca87e46357904ed3bcebf19902feea
wang-zhenqi <dreamingwzq@gmail.com>
2020-10-04 09:33:55 +0800

    [modify] version 2 of problem 32 - II of LCOF. DFS.

--- LCOF/src/PrintBinaryTree_32_II.java
+++ LCOF/src/PrintBinaryTree_32_II.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
@@ -106,31 +103,42 @@ public class PrintBinaryTree_32_II {
     }
 
     /*
-     * Version 1, BFS.
+     * Version 2, DFS.
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root == null) {
             return new ArrayList<>();
         }
-        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
         List<List<Integer>> result = new ArrayList<>();
-        queue.offer(root);
-
+        HashSet<TreeNode> visited = new HashSet<>();
+        stack.push(root);
+        int curLevel = 0;
         do {
-            int length = queue.size();
-            List<Integer> level = new ArrayList<>();
-            for(int i = 0; i < length; i++) {
-                TreeNode curNode = queue.poll();
-                level.add(curNode.val);
-                if(curNode.left != null) {
-                    queue.offer(curNode.left);
+            TreeNode curNode = stack.peek();
+            if(!visited.contains(curNode)) {
+                if(curLevel + 1 > result.size()) {
+                    List<Integer> level = new ArrayList<>();
+                    level.add(curNode.val);
+                    result.add(level);
+                } else {
+                    result.get(curLevel).add(curNode.val);
                 }
-                if(curNode.right != null) {
-                    queue.offer(curNode.right);
-                }
+                visited.add(curNode);
             }
-            result.add(level);
-        } while(!queue.isEmpty());
+
+            if(curNode.left != null && !visited.contains(curNode.left)) {
+                stack.push(curNode.left);
+                curLevel++;
+            } else if(curNode.right != null && !visited.contains(curNode.right)) {
+                stack.push(curNode.right);
+                curLevel++;
+            } else {
+                stack.pop();
+                curLevel--;
+            }
+        } while(!stack.isEmpty());
+
         return result;
     }
 
