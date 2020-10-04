3b2e7e8c68f4a8c6f4a5bd04c91575493fdc4c8c
wang-zhenqi <dreamingwzq@gmail.com>
2020-10-04 09:20:21 +0800

    [add] problem 32 - II of LCOF, BFS. The same as problem 102.

---
+++ LCOF/src/PrintBinaryTree_32_II.java
@@ -0,0 +1,147 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * <p>
+ * 提示：
+ * 节点总数 <= 1000
+ * <p>
+ * 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PrintBinaryTree_32_II {
+    private static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if(input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if(index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if(!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if(index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if(!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    private static String integerArrayListToString(List<Integer> nums, int length) {
+        if(length == 0) {
+            return "[]";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result.append(number).append(", ");
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    private static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    private static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for(List<Integer> list : nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            List<List<Integer>> ret = new PrintBinaryTree_32_II().levelOrder(root);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    /*
+     * Version 1, BFS.
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) {
+            return new ArrayList<>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        queue.offer(root);
+
+        do {
+            int length = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < length; i++) {
+                TreeNode curNode = queue.poll();
+                level.add(curNode.val);
+                if(curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if(curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            result.add(level);
+        } while(!queue.isEmpty());
+        return result;
+    }
+
+    // Definition for a binary tree node.
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
