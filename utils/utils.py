65ddc74b246fec8d1104e02a5857b5dc2311d992
wang-zhenqi <dreamingwzq@gmail.com>
2024-10-12 21:32:41 +0800

    feat: update utils and poetry

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,10 @@
-from datetime import time
+import sys
+import time
+
+import loguru
+
+loguru.logger.remove()
+loguru.logger.add(sys.stdout, level="INFO")
 
 
 def running_time(func):
@@ -6,7 +12,17 @@ def running_time(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(f"Total running time: {end - start:.2f} seconds")
+        loguru.logger.info(f"Total running time: {end - start:.2f} seconds")
         return result
 
     return wrapper
+
+
+if __name__ == "__main__":
+
+    @running_time
+    def test():
+        for i in range(1000000):
+            pass
+
+    test()
