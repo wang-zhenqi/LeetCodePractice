f6a7e81f55b00a218956b99d725663850ef321ff
wang-zhenqi <dreamingwzq@gmail.com>
2025-09-29 00:01:54 +0800

    [chore] update the fixture params

--- DataStructure/tests/conftest.py
+++ DataStructure/tests/conftest.py
@@ -5,21 +5,21 @@ from DataStructure.queues import ContainerConfig, Queue
 
 @pytest.fixture(
     params=[
-        lambda: Queue(config=ContainerConfig()),
-        lambda: Queue(config=ContainerConfig(max_size=10)),
-        lambda: Queue(config=ContainerConfig(max_size=9, variation="circular")),
-        lambda: Queue(config=ContainerConfig(variation="double_ended")),
-        lambda: Queue(config=ContainerConfig(max_size=8, variation="double_ended")),
-        lambda: Queue(config=ContainerConfig(implementation="linked")),
-        lambda: Queue(config=ContainerConfig(max_size=7, implementation="linked")),
-        lambda: Queue(config=ContainerConfig(max_size=6, implementation="linked", variation="circular")),
-        lambda: Queue(config=ContainerConfig(implementation="linked", variation="double_ended")),
-        lambda: Queue(config=ContainerConfig(max_size=5, implementation="linked", variation="double_ended")),
-        lambda: Queue(config=ContainerConfig(implementation="deque")),
-        lambda: Queue(config=ContainerConfig(max_size=4, implementation="deque")),
-        lambda: Queue(config=ContainerConfig(max_size=3, implementation="deque", variation="circular")),
-        lambda: Queue(config=ContainerConfig(implementation="deque", variation="double_ended")),
-        lambda: Queue(config=ContainerConfig(max_size=10, implementation="deque", variation="double_ended")),
+        Queue(config=ContainerConfig()),
+        Queue(config=ContainerConfig(max_size=10)),
+        Queue(config=ContainerConfig(max_size=9, variation="circular")),
+        Queue(config=ContainerConfig(variation="double_ended")),
+        Queue(config=ContainerConfig(max_size=8, variation="double_ended")),
+        Queue(config=ContainerConfig(implementation="linked")),
+        Queue(config=ContainerConfig(max_size=7, implementation="linked")),
+        Queue(config=ContainerConfig(max_size=6, implementation="linked", variation="circular")),
+        Queue(config=ContainerConfig(implementation="linked", variation="double_ended")),
+        Queue(config=ContainerConfig(max_size=5, implementation="linked", variation="double_ended")),
+        Queue(config=ContainerConfig(implementation="deque")),
+        Queue(config=ContainerConfig(max_size=4, implementation="deque")),
+        Queue(config=ContainerConfig(max_size=3, implementation="deque", variation="circular")),
+        Queue(config=ContainerConfig(implementation="deque", variation="double_ended")),
+        Queue(config=ContainerConfig(max_size=10, implementation="deque", variation="double_ended")),
     ],
     ids=[
         "DynamicArray",
@@ -41,4 +41,4 @@ from DataStructure.queues import ContainerConfig, Queue
     scope="module",
 )
 def queue_(request):
-    return request.param()
+    return request.param
